93ad665dca5e48d7275290360add3571ea552542
Vladimir <user4965@MacBook-Air-Vladimir.local>
2026-05-05 15:24:56 +0800

    added format option to gendiff, cli

---
+++ gendiff/cli.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument('first_file', type=str, help='Path to the first file')
+    parser.add_argument('second_file', type=str, help='Path to the second file')
+    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output',)
+    return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,9 @@
-from argparse import ArgumentParser
+from gendiff.cli import parse_args
 
 
 def main():
-    parser = ArgumentParser(description="Compares two configuration files and shows a difference.")
-    parser.add_argument('first_file', type=str, help='Path to the first file')
-    parser.add_argument('second_file', type=str, help='Path to the second file')
-    parser.parse_args()
+    parse_args()
+
 
 if __name__ == '__main__':
     main()
