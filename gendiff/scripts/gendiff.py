eae6c6366020a91cf92e1c0c6aa7e4901129eb3d
Vladimir <user4965@MacBook-Air-Vladimir.local>
2026-05-06 20:11:53 +0800

    added json file parsing

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,20 @@
+import json
 from gendiff.cli import parse_args
 
 
 def main():
-    parse_args()
+    args = parse_args()
+
+    dict1 = load_json(args.first_file)
+    dict2 = load_json(args.second_file)
+
+    print(dict1)
+    print(dict2)
+
+
+def load_json(path):
+    with open(path) as file:
+        return json.load(file)
 
 
 if __name__ == '__main__':
